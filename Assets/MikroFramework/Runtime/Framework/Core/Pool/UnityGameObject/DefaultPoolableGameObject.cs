ab3bc33f6842d10a27fb243112db0015609cc0e3
cty288 <cty288@163.com>
2022-01-19 22:22:07 -0500

    initial

---
+++ Assets/MikroFramework/Runtime/Framework/Core/Pool/UnityGameObject/DefaultPoolableGameObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MikroFramework.Pool;
+using UnityEngine;
+
+namespace MikroFramework.ResKit
+{
+    public class DefaultPoolableGameObject: PoolableGameObject
+    {
+        public override void OnInit() {
+            
+        }
+
+        public override void OnRecycled() {
+            transform.SetParent(Pool.transform);
+        }
+    }
+}
