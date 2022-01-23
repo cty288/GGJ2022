ecefabbe81c41e34a5ed506bbf9e6ff2078846cd
Alex020128 <alex20020128@gmail.com>
2022-01-23 07:48:39 -0500

    Title; Transition

---
+++ Assets/01. Scripts/Level1to2/comicPlay.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class comicPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+    public GameObject blackObject;
+
+    public GameObject hint;
+    private TMP_Text text;
+
+    public Image image1;
+    public Image image2;
+    public Image image3;
+    public Image black;
+
+    public bool image1out = false;
+    public bool image2out = false;
+    public bool image3out = false;
+
+    public bool image3turn = false;
+    public bool gameout = false;
+
+    void Start()
+    {
+        object1 = GameObject.Find("Image1");
+        object2 = GameObject.Find("Image2");
+        object3 = GameObject.Find("Image3");
+        blackObject = GameObject.Find("Black");
+
+        hint = GameObject.Find("Hint");
+
+        Image image1 = object1.GetComponent<Image>();
+        Image image2 = object2.GetComponent<Image>();
+        Image image3 = object3.GetComponent<Image>();
+        Image black = blackObject.GetComponent<Image>();
+
+        text = hint.GetComponent<TMP_Text>();
+        text.enabled = false;
+
+        StartCoroutine(WaitForSecs());
+    }
+
+    IEnumerator WaitForSecs()
+    {
+        yield return new WaitForSeconds(1f);
+        image1.DOFade(1, 1);
+        image1out = true;
+        text.enabled = true;
+    }
+    IEnumerator WaitForSecs2()
+    {
+        yield return new WaitForSeconds(1f);
+        image3turn = true;
+    }
+    IEnumerator WaitForSecs3()
+    {
+        yield return new WaitForSeconds(1f);
+        gameout = true;
+    }
+
+    IEnumerator WaitForSecs4()
+    {
+        yield return new WaitForSeconds(1f);
+        text.enabled = false;
+        black.DOFade(1, 1);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(image1out == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                image2.DOFade(1, 1);
+                image2out = true;
+                StartCoroutine(WaitForSecs2());
+            }
+        }
+
+        if ((image2out == true) &&(image3turn == true))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                image3.DOFade(1, 1);
+                image3out = true;
+                StartCoroutine(WaitForSecs3());
+            }
+        }
+
+        if((image3out == true) && (gameout == true))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(WaitForSecs4());
+            }
+        }
+
+    }
+}
---
+++ Assets/01. Scripts/Level3/Unlock.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using MikroFramework.Event;
+public struct Success
+{
+
+}
+
+
+public class Unlock : MonoBehaviour
+{
+
+    public Transform clipIns;
+    public Transform keyHole;
+    public SpriteRenderer stickRenderer;
+    public SpriteRenderer repairRenderer;
+    public SpriteRenderer tutorialRenderer;
+    public Sprite stickAva;
+    public Sprite stcikBreak;
+    public Text countText;
+    public float rotError = 5f;
+    public float rotSpeed = .4f;
+    public float resumeDuration = .4f;
+    public float moveFullDuration = 1f;
+    public float repairEffectFadeTime = 2f;
+    public int stickLastingCount = 3;
+
+    float targetRotation;
+
+    bool gameFinish = false;
+    bool corretAngle = false;
+    int stickLasting = 3;
+
+    void Start()
+    {
+        stickLasting = stickLastingCount;
+        targetRotation = Random.Range(rotError - 90f, 90f - rotError);
+        Debug.Log(targetRotation);
+        TypeEventSystem.RegisterGlobalEvent<CorrectAction>(ResumeStick);
+    }
+
+    void ResumeStick(CorrectAction action)
+    {
+        if (stickLasting == 0)
+        {
+            stickRenderer.sprite = stickAva;
+            stickLasting = stickLastingCount;
+            //如果有的话，播放修复发卡的音效
+            PlayClip(11);
+            repairRenderer.DOFade(1, 0);
+            repairRenderer.DOFade(0, repairEffectFadeTime);
+        }
+    }
+
+    void Update()
+    {
+        countText.text = stickLasting.ToString();
+        if (tutorialRenderer.color.a == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E)) tutorialRenderer.DOFade(0, repairEffectFadeTime);
+        }
+        if (!gameFinish)
+        {
+            float transferZ = clipIns.rotation.eulerAngles.z;
+            if (transferZ > 270 - 11.4514) transferZ -= 360;
+            if (stickLasting != 0)
+            {
+                if (Input.GetKey(KeyCode.Q) && transferZ <= 90)
+                {
+                    clipIns.Rotate(new Vector3(0, 0, rotSpeed));
+                }
+                if (Input.GetKey(KeyCode.E) && transferZ >= -90)
+                {
+                    clipIns.Rotate(new Vector3(0, 0, -rotSpeed));
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    float solveE = SolveError();
+                    //在这里播放转动的音效
+                    if (Random.Range(0, 1f) > 0.5f) PlayClip(3); else PlayClip(4);
+                    keyHole.DORotate(new Vector3(0, 0, solveE - 90), moveFullDuration);
+                    Invoke("FinishRotate", moveFullDuration);
+                }
+            }
+            if (!Input.GetKey(KeyCode.Space)) ResumeLock();
+        }
+    }
+
+    void FinishRotate()
+    {
+        if (corretAngle) UnlockSuccess();
+        else UnlockFail();
+    }
+
+    void ResumeLock()
+    {
+        keyHole.DORotate(Vector3.zero, resumeDuration);
+        CancelInvoke("FinishRotate");
+    }
+
+    float SolveError()
+    {
+        float transferZ = clipIns.rotation.eulerAngles.z;
+        if (transferZ > 270 - 11.4514) transferZ -= 360;
+        float ret = Mathf.Abs(transferZ - targetRotation);
+        corretAngle = false;
+        if (ret < rotError)
+        {
+            corretAngle = true;
+            return 0;
+        }
+        return ret/2;
+    }
+
+    void UnlockSuccess()
+    {
+        //在这里播放成功开锁音效
+        PlayClip(8);
+        GetComponent<Animation>().Play();
+        gameFinish = true;
+        Success success = new Success();
+        TypeEventSystem.SendGlobalEvent(success);
+    }
+
+    void UnlockFail()
+    {
+        ShakeCamera.Shake(true, 5, 35, 0.3f);
+        //在这里要播放未成功开锁音效
+        PlayClip(0);
+        stickLasting--;
+        if (stickLasting == 0)
+        {
+            Invoke("BreakStick", resumeDuration);
+        }
+    }
+
+    void BreakStick()
+    {
+        //在这里播放棍子断掉的音效
+        PlayClip(10);
+        stickRenderer.sprite = stcikBreak;
+    }
+
+    void PlayClip(int index)
+    {
+        PlayAudio playAudio = new PlayAudio();
+        playAudio.index = index;
+        TypeEventSystem.SendGlobalEvent(playAudio);
+    }
+}
